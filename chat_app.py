42d0b3e26856777ab5e6aa74ed31d7a99f1fcb72
Shyam Sreevalsan <shyam.valsan@gmail.com>
2024-07-19 13:01:10 +0300

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_feedback import streamlit_feedback
-from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.core import VectorStoreIndex,SimpleDirectoryReader,ServiceContext,PromptTemplate,Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
